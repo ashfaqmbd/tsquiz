0057fed10f5893226d6a643bf52ad6cd69cc9a77
Ashfaq Ahmed <ashfaqmbd@gmail.com>
2020-08-28 10:38:57 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -49,7 +49,7 @@ else
       //const quest=questions[number].question;
       if (correct) setScore(prev => prev + 1)
       const AnswerObject = {
-       question: questions[number].question,
+       questions: questions[number].question,
        answer,
         correct,
         correctAnswer: questions[number].correct_answer
